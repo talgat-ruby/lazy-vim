397d7c4e7e33702f6e2888609b359627dc643bec
TalgatS <talgat.s@pm.me>
2025-11-20 13:29:30 +0500

    scroll with <Tab> and <S-Tab> during autocomplete

---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      },
+    },
+  },
+}
