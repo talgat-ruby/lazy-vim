fbd5cbeef7b8031ca18110a40505099cd75900bc
TalgatS <talgat.s@pm.me>
2025-11-20 13:28:36 +0500

    add <C-S-h> and <C-S-l> to move buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete default [b and ]b buffer navigation
+vim.keymap.del("n", "[b")
+vim.keymap.del("n", "]b")
+
+-- Add new [c and ]c buffer navigation
+vim.keymap.set("n", "<C-S-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer prev" })
+vim.keymap.set("n", "<C-S-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer next" })
